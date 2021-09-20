6a0be9647c3bcc630ff2ab942f5a4056b1b376e2
Aadhi-N <aadhi.nizamdeen@gmail.com>
2021-09-20 17:30:32 -0400

    data loading condition added for courseCard

--- pages/recommendations/index.js
+++ pages/recommendations/index.js
@@ -14,41 +14,43 @@ import CourseCard from "../course/courseCard";
 const Recommendations = () => {
     const { data, error } = useSWR("/api/recommendations", fetcher);
     if (error) return <div>failed to load</div>
-    if (!data) return <div>loading...</div>
-
-    return (
-        <Container>
-            <HomepageHeaders>Recommended Courses</HomepageHeaders>
-            <LargeTilesContainer>
-                {data.map(item => {
-                    return (
-                        <Tile>
-                            <Link
-                                href={{
-                                    pathname: "course/[id]",
-                                    query: {
-                                        id: item.id,
-                                        title: item.title
-                                    }
-                                }}
-                                as={`recommended/course/${item.id}-${urlSlug(item.title)}`} // generate dynamic URL
-                            >
-                                <AnchorTag>
-                                    <CourseCard 
-                                        imageURL={item.image} 
-                                        title={item.title} 
-                                        description={item.description}
-                                        lessons={item.lessons} 
-                                        duration={item.duration} 
-                                    />
-                                </AnchorTag>
-                            </Link>
-                        </Tile>
-                    )
-                })}
-            </LargeTilesContainer>
-        </Container>
-    )   
+    if (!data) {
+        return <div>loading...</div>
+    } else {
+        return (
+            <Container>
+                <HomepageHeaders>Recommended Courses</HomepageHeaders>
+                <LargeTilesContainer>
+                    {data.map(item => {
+                        return (
+                            <Tile>
+                                <Link
+                                    href={{
+                                        pathname: "course/[id]",
+                                        query: {
+                                            id: item.id,
+                                            title: item.title
+                                        }
+                                    }}
+                                    as={`recommended/course/${item.id}-${urlSlug(item.title)}`} // generate dynamic URL
+                                >
+                                    <AnchorTag>
+                                        <CourseCard 
+                                            imageURL={item.image} 
+                                            title={item.title} 
+                                            description={item.description}
+                                            lessons={item.lessons} 
+                                            duration={item.duration} 
+                                        />
+                                    </AnchorTag>
+                                </Link>
+                            </Tile>
+                        )
+                    })}
+                </LargeTilesContainer>
+            </Container>
+        )   
+    }
 };
 
 export default Recommendations;
